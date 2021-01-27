52a5f896fd70212b7c1bc0e7c0e9e44d96438f07
KyuubiRan <KyuubiRan@qq.com>
2021-01-27 13:35:08 +0800

    Add hint

--- src/main/kotlin/listener/InitListener.kt
+++ src/main/kotlin/listener/InitListener.kt
@@ -2,8 +2,12 @@ package listener
 
 class InitListener {
     init {
+        //此处自定义Listener 不需要的可以注释掉
+        //on/off指令
         TogglePush()
+        // status 指令(/s)
         GetStatus()
+        //帮助指令(/h)
         HelpMsg()
     }
 }
